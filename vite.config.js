44f3bde0de45e6f058561f8b45c4081e266bcf78
pranav514 <pranavsalunkhe327@gmail.com>
2024-04-20 13:39:22 +0530

    added env

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import dotenv from "dotenv"
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server : {
     proxy:{
-      "/api/": "https://medical-trial-wt.onrender.com/",
-      "/uploads/": "https://medical-trial-wt.onrender.com/",
+      "/api/": process.env.BACKEND_URL,
+      "/uploads/": process.env.BACKEND_UPLOAD_URL,
       
     }
   }
